f4a299f6f9ff1fbb8b4824c56f204647955f1d7b
firstmentor <jainvikas887@gmail.com>
2021-06-16 19:31:42 +0530

    hrllo

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'pninfosys';
+
+  name ='vikas jain';
+  college='RJIT';
+  phone =7000846823;
+}
